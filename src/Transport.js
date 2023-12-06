3e52d27ed3f934568a1c07b920183c8c889ad0ae
comptergeeks <F@rhan0108>
2023-12-06 14:48:16 -0800

    fixed josephs last name

--- src/Transport.js
+++ src/Transport.js
@@ -12,7 +12,7 @@ function Tech() {
            <video controls autoPlay loop muted> 
            <source src={myVideo} type="video/mp4"></source>
            </video>
-           <h3>By: Joseph Parris</h3>
+           <h3>By: Joseph Paris</h3>
            <p>In Columbia City, transportation was both plentiful and accessible. The area has its own light rail station that’s a short walk to many prominent locations in the city. In addition to having its own light rail station, Columbia City has plenty of buses and bus stops throughout the area.</p>
            <p>The area is also a growing suburb with many new property developments, including apartments and townhouses. Businesses were also plentiful in the area, with many restaurants and shops in their downtown area. Our experience matches up with the research I’ve done on the area, especially regarding establishing new housing and Columbia City’s development as a suburb.</p>
            </div>
